8e4c09c251e043417dcfb93cb16f4f3ab2e0f96e
Luis Gomez <00405018@uca.edu.sv>
2019-11-18 12:04:11 -0600

    Agregue el programa en js y termine de colocar los elementos en el html

---
+++ app/app3.js
@@ -0,0 +1,16 @@
+function fact(n){
+    f=1;
+    
+    for (i=1;i <n+1; i++){
+        f=f*i 
+    };
+    return f 
+}
+
+function bin(){
+    k = parseInt(document.datos.k.value);
+    n = parseInt(document.datos.n.value);
+    p = parseFloat(document.datos.p.value);
+    
+    document.datos.prob.value = Math.round((fact(n)/(fact(n-k)*fact(k))*Math.pow(p,k)*Math.pow(1-p,n-k))*100) + "%"; 
+}
