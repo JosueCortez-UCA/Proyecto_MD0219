589d4e09e0ad92aa86ff6184e3f02107617773b5
JosueCortez-UCA <00402918@uca.edu.sv>
2019-11-18 23:10:45 -0600

    ADD: solucion app4

--- app/app4.js
+++ app/app4.js
@@ -1,56 +1,39 @@
-let numero = document.querySelector("#factorial");
-let btn_calcular = document.querySelector("#app4-btn");
-let div_impresion = document.querySelector("#App4 div.impresion");
+let numero_app4 = document.querySelector("#factorial");
+let btn_calcular_app4 = document.querySelector("#app4-btn");
+let div_impresion_app4 = document.querySelector("#App4 div.impresion");
 
+btn_calcular_app4.addEventListener("click", () => {
+    let numero = numero_app4.value;
 
+    if (numero/numero) {
+        imprimir_app4(numero, factorial(numero));
+    }
+    else {
+        alert("Debe ingresar un numero");
+    }
+});
 
 function factorial(numero) {
 	resultado = 1;
-	for (var i = 1; i <= numero; i++) {
+
+    for (var i = 1; i <= numero; i++) {
 		resultado *= i;
 	}
 	return resultado;
-}
+};
 
+let imprimir_app4 = (numero, resultado) => {
+    let objeto_impresion = document.createElement("div");
+    objeto_impresion.className = "resultado";
+    objeto_impresion.id = "impresion_app4";
 
-/*<html>
- 
- <head>
-  
- <title>Factorial</title>
-  
- <script language="javascript">
-  
- function factorial (){
-     var factorial = 1;
-  
-         for (var i=1; i <= document.getElementById ('n').value; i++){
-             factorial *= i;
-         }
-  
-         document.getElementById ('resultado').value = factorial;
- }
-  
- </script>
-  
- </head>
-  
- <body>
-  
- <form onsubmit="javascript: factorial ();return (false);">
-  
- Número: <input type="text" name="n" id="n" />
-  
- <br />
-  
- Factorial: <input type="text" name="resultado" id="resultado" />
-  
- <br />
-  
- <input type="submit" value="Calcular" />
-  
- </form>
-  
- </body>
-  
- </html>*/
+    objeto_impresion.innerHTML = `
+        <hr class="linea_bonita">
+        <h4>
+            Resultado <br/>
+            <small class="text-muted">El factorial de ${numero}! es ${resultado} </small>
+        </h4>
+    `;
+
+    div_impresion_app4.replaceChild(objeto_impresion, document.getElementById("impresion_app4"));
+};
